e337572da32c74d254ffb569caff95bcf1086e97
endlesSWE <svetogor.illya@gmail.com>
2023-07-02 11:43:19 +0300

    hm5

--- src/core/Main.java
+++ src/core/Main.java
@@ -8,6 +8,7 @@ public class Main {
         Lecture lecture = new Lecture("Algebra",1);
         Lecture lecture1 = new Lecture("Mechanics",2);
         Lecture lecture2 = new Lecture("Geometry",3);
+        Lecture lecture3 = new Lecture("Literature",4);
 
         Lecture.showLectureCount();
     }
