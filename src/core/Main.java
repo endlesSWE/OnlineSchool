9d7fa0e33d6ec4489db59fd1be5418cf77d69736
endlesSWE <svetogor.illya@gmail.com>
2023-07-02 11:42:08 +0300

    hm5

--- src/core/Main.java
+++ src/core/Main.java
@@ -1,6 +1,7 @@
 package core;
 
 import core.entities.Lecture;
+import core.utils.LectureUtils;
 
 public class Main {
 
@@ -8,6 +9,8 @@ public class Main {
         Lecture lecture = new Lecture("Algebra",1);
         Lecture lecture1 = new Lecture("Mechanics",2);
         Lecture lecture2 = new Lecture("Geometry",3);
+        Lecture lecture3 = new Lecture("Art", 4);
+        Lecture lecture4 = new Lecture("History", 5);
 
         Lecture.showLectureCount();
     }
